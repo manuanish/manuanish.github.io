76b069fb1ec54790148bb510907d00700985be29
Manu Anish <54843973+manuanish@users.noreply.github.com>
2022-06-17 10:16:13 -0400

    prettier + rounded images

--- components/blog/ELADemo.js
+++ components/blog/ELADemo.js
@@ -1,38 +1,77 @@
 import Image from "next/image";
 import * as React from "react";
-import {Slider} from "@geist-ui/core";
+import { Slider } from "@geist-ui/core";
 
 export default function ELADemo() {
-  const [normalOpacity, setNormalOpaicty] = React.useState(1)
-  const [elaOpacity, setElaOpaicty] = React.useState(0)
-  const [value, setValue] = React.useState(0)
+  const [normalOpacity, setNormalOpaicty] = React.useState(1);
+  const [elaOpacity, setElaOpaicty] = React.useState(0);
+  const [value, setValue] = React.useState(0);
 
-  const handleSlider = value => {
-    console.log(value)
-    setValue(value)
-    setElaOpaicty(value / 100)
-    setNormalOpaicty((100 - value) / 100)
-  }
+  const handleSlider = (value) => {
+    console.log(value);
+    setValue(value);
+    setElaOpaicty(value / 100);
+    setNormalOpaicty((100 - value) / 100);
+  };
 
-  return(
+  return (
     <div>
+      <br/>
       <div className="flex justify-center">
-        <div style={{position: 'relative', top: 0, left: 0}}>
-          <div style={{position: 'relative', top: 0, left: 0, opacity: normalOpacity}}>
-            <Image src="/blog/ela-demo/normal.png" width="503" height="509" />
+        <div style={{ position: "relative", top: 0, left: 0, borderRadius: 10 }}>
+          <div
+            style={{
+              position: "relative",
+              top: 0,
+              left: 0,
+              opacity: normalOpacity,
+              borderRadius: 10
+            }}
+          >
+            <Image
+              src="/blog/ela-demo/normal.png"
+              width="503"
+              height="509"
+              quality={100}
+              placeholder={"blur"}
+              blurDataURL
+              style={{ borderRadius: 10 }}
+            />
           </div>
-          <div style={{position: 'absolute', top: 0, left: 0, opacity: elaOpacity}}>
-            <Image src="/blog/ela-demo/ela.png" width="503" height="509" />
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              opacity: elaOpacity,
+              borderRadius: 10
+            }}
+          >
+            <Image
+              src="/blog/ela-demo/ela.png"
+              width="503"
+              height="509"
+              quality={100}
+              placeholder={"blur"}
+              blurDataURL
+              style={{ borderRadius: 10 }}
+            />
           </div>
         </div>
       </div>
-      <br/>
+      <br />
       <div className="flex justify-center">
         <div className="min-w-[300px] max-w-[600px]">
-          <Slider min={0} max={100} step={1} value={value} onChange={handleSlider}/>
+          <Slider
+            min={0}
+            max={100}
+            step={1}
+            value={value}
+            onChange={handleSlider}
+          />
         </div>
       </div>
-      <br/>
+      <br />
     </div>
   );
 }
