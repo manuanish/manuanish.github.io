730d380a38dfc0ae92666a8f7faf617bb2265ab1
Manu Anish <54843973+manuanish@users.noreply.github.com>
2022-06-17 09:23:31 -0400

    Mostly done...

---
+++ components/blog/ELADemo.js
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import * as React from "react";
+import {Slider} from "@geist-ui/core";
+
+export default function ELADemo() {
+  const [normalOpacity, setNormalOpaicty] = React.useState(1)
+  const [elaOpacity, setElaOpaicty] = React.useState(0)
+  const [value, setValue] = React.useState(0)
+
+  const handleSlider = value => {
+    console.log(value)
+    setValue(value)
+    setElaOpaicty(value / 100)
+    setNormalOpaicty((100 - value) / 100)
+  }
+
+  return(
+    <div>
+      <div className="flex justify-center">
+        <div style={{position: 'relative', top: 0, left: 0}}>
+          <div style={{position: 'relative', top: 0, left: 0, opacity: normalOpacity}}>
+            <Image src="/blog/ela-demo/normal.png" width="503" height="509" />
+          </div>
+          <div style={{position: 'absolute', top: 0, left: 0, opacity: elaOpacity}}>
+            <Image src="/blog/ela-demo/ela.png" width="503" height="509" />
+          </div>
+        </div>
+      </div>
+      <br/>
+      <div className="flex justify-center">
+        <div className="min-w-[300px] max-w-[600px]">
+          <Slider min={0} max={100} step={1} value={value} onChange={handleSlider}/>
+        </div>
+      </div>
+    </div>
+  );
+}
